dce13a7e3cfab2e9d7eb86d14a8352490df08f3e
Jeremy Bruzac <bruzacj@yahoo.fr>
2022-11-21 17:14:23 +0100

    add test volum sphere

--- src/app-sphere.js
+++ src/app-sphere.js
@@ -5,6 +5,11 @@ class Sphere {
   getRadius() {
     return this.diameter / 2;
   }
+  getVolume() {
+    let radius = this.getRadius();
+    radius = Math.abs(radius);
+    return Math.round((4 / 3) * Math.PI * Math.pow(radius, 3) * 100) / 100;
+  }
 }
 
 module.exports = {
--- test/app.sphere.test.js
+++ test/app.sphere.test.js
@@ -2,9 +2,15 @@ const Sphere = require("../src/app-sphere").Sphere;
 const expect = require("chai").expect;
 
 describe("Testing the Sphere Functions", function () {
-  it("1. The radius of the Circle", function (done) {
+  it("1. The radius of the Sphere", function (done) {
     let s1 = new Sphere(20);
     expect(s1.getRadius()).to.equal(10);
+    done();
+  });
+
+  it("2. The volume of the Sphere", function (done) {
+    let s2 = new Sphere(20);
+    expect(s2.getVolume()).to.equal(4188.79);
     done();
   });
 });
